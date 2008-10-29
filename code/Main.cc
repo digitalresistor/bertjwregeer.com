1a804d2319d1b8b5001629670dbfe8a50847d76d
Bert JW Regeer <bertjw@regeer.org>
2008-10-29 15:13:02 +0000

    Moved all the code to it's own directory. Now I can develop just the website there
    git-svn-id: http://svn.0x58.com/repos/BertJWRegeer/Semesters/Fall%202008/PRO210@9 3d59c550-c999-dd11-821d-0030482bb012

---
+++ code/Main.cc
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2008 Bert JW Regeer;
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+**/
+
+#include <iostream>
+#include <unistd.h>
+
+int main () {
+	std::cout << "Welcome to Berts website" << std::endl;
+	std::cout << "\t1. About me" << std::endl;
+	std::cout << "\t2. Projects" << std::endl;
+	std::cout << "\t3. Resume" << std::endl;
+	std::cout << "\t4. Contact" << std::endl;
+
+	int input;
+
+	std::cout << "Enter your selection: " << std::flush;
+        std::cin >> input;
+
+        // We assume that execl will not fail!
+	switch (input) {
+	        case 1: execl("./About me", "About me", (char *)0);
+                case 2: execl("./Projects", "Projects", (char *)0);
+                case 3: execl("./Resume", "Resume", (char *)0);
+                case 4: execl("./Contact", "Contact", (char *)0);
+                default:
+                        std::cerr << "Bad selection" << std::endl;
+                        return 1;
+	}
+
+        return 0;
+}
---
+++ code/includes/Person.h
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2008 Bert JW Regeer;
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+**/
+#ifndef _PERSON_H_
+#define _PERSON_H_
+
+#include <iostream>
+
+typedef struct education_container
+{
+        std::string schoolname;
+        std::string degree;
+        std::string major;
+} Education;
+
+class Person  {
+        public:
+                Person (std::string * name, std::string * biography, Education * education) : _name(name), _biography(biography), _education(education) { };
+                ~Person ();
+        
+                friend std::ostream& operator<< (std::ostream& o, const Person& person);
+
+        private:
+                std::string * _name;
+                std::string * _biography;
+                Education   * _education;
+};
+
+std::ostream& operator<< (std::ostream& o, const Person& person) {
+        o << "Name: " << *(person._name) << std::endl;
+        o << "Biography: " << std::endl << *(person._biography) << std::endl;
+        o << "Education: " << std::endl;
+        o << "\tSchool: " << (*(person._education)).schoolname << std::endl;
+        o << "\tDegree: " << (*(person._education)).degree << std::endl;
+        o << "\tMajor: " << (*(person._education)).major << std::endl;
+        
+        return o;
+}
+
+#endif /* _PERSON_H_ */
