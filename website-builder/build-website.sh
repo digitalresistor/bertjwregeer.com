0ce6ec09a3d56d2a6a8325ef273f9e595bfd14de
Bert JW Regeer <bertjw@regeer.org>
2008-11-12 23:40:17 +0000

    Updated the website builder, as well as the syntax highlighter
    git-svn-id: http://svn.0x58.com/repos/BertJWRegeer/Semesters/Fall%202008/PRO210@24 3d59c550-c999-dd11-821d-0030482bb012

--- website-builder/build-website.sh
+++ website-builder/build-website.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-if [ ! 1 -eq $# ]; then
-	echo "Please provide a path to the file you want to process, and the location to place the processed file!"
+if [ ! 2 -eq $# ]; then
+	echo "$1 <path to code> <path to output>"
 	exit
 fi
 
+if [ ! -d $2 ]; then
+	echo "Output directory does not exist, creating ..."
+	mkdir $2
+fi
+
+for I in `find $1 -type d | grep -v ".svn"`; do
+	echo ${I#$1};
+	
+	if [ ! -d $2/${I#$1} ]; then
+		echo "Creating directory $2${I#$1}"
+		mkdir $2/${I#$1}
+	fi
+	
+	for J in `find $I -type f -depth 1 | grep -v ".svn" | grep -v ".DS_Store" | grep -v "Makefile"`; do
+		echo -e "\t ${J#$1}"
+		./create-page.sh ${J} $2/${I#$1}
+	done
+done
