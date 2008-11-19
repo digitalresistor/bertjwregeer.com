b15c97a8ebd1905802dfbf94490a2272b944182c
Bert JW Regeer <bertjw@regeer.org>
2008-11-19 12:11:11 +0000

    Fix the tabbing issue. Soft tabs need to go away ... also finally add in an actual About Me piece\! This is not live yet.
    git-svn-id: http://svn.0x58.com/repos/BertJWRegeer/Semesters/Fall%202008/PRO210@30 3d59c550-c999-dd11-821d-0030482bb012

--- code/About_me.cc
+++ code/About_me.cc
@@ -20,27 +20,41 @@
 #include "includes/Person.h"
 
 int main () {
-        std::string * name, * biography;
-        Education * education;
-        Person * Bert;
-        
-        name = new std::string("Bert JW Regeer");
-        // TODO: Actually write something about yourself!
-        biography = new std::string("Please be patient as I am still updating my website!");
-                    
-        education = new Education;
-        education->schoolname = "{University of Advancing Technology}[http://uat.edu]";
-        education->degree     = "Bachelor of Science: Software Engineering";
-        education->major      = "Programming";
-        
-        Bert = new Person(name, biography, education);
+	std::string * name, * biography;
+	Education * education;
+	Person * Bert;
 
-        // Print the results to the screen
-        std::cout << (*Bert) << std::endl << std::flush;
-        
-        delete Bert;
+	name = new std::string("Bert JW Regeer");
+	biography = new std::string(
+		"I am a current student at the {University of Advancing Technology}[http://uat.edu] studying software engineering. I am "
+		"currently nearing completion of my degree with only two more semesters left at UAT.\r\n"
+		
+		"I am a highly motivated individual, who enjoys working with technology. My strongest points are my "
+		"expertise in UNIX/Linux server administration and programming for those platforms. I enjoy working "
+		"with open source software and writing my own software. I am currently part of several {projects}[./Projects.html] at "
+		"UAT and I am also working on several of my own projects.\r\n"
+		
+		"I participate in various clubs and groups around school and am active in the Network Security "
+		"scene, having gone to both Black Hat and DefCon. I also enjoy going to the lesser known Net sec "
+		"groups. HATS is local to UAT, and ToorCon which is held in San Diego.\r\n"
+		
+		"I am from the Netherlands, and as such bring a unique perspective to any team. Having lived in and "
+		"travelled to many countries I am very open to new cultures and ideas. I have resided in the US for "
+		"the last 8 years, and  during that time have met many extra-ordinary and awesome people that I have "
+		"had the pleasure of working with."
+		);
+
+	education = new Education;
+	education->schoolname = "{University of Advancing Technology}[http://uat.edu]";
+	education->degree     = "Bachelor of Science: Software Engineering";
+	education->major      = "Programming";
+
+	Bert = new Person(name, biography, education);
+
+	// Print the results to the screen
+	std::cout << (*Bert) << std::endl << std::flush;
 
 	// Re-run the menu application to give the visitor more selections: 
 	execl("./Main", "{Main}[./index.html]", (char *)0);
-        return 0;
+	return 0;
 }
